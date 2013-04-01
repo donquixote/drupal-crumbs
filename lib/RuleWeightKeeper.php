1621c4e2b3efd2d4338930301da086b56a0306ad
Andreas Hennings <andreas@dqxtech.net>
2013-04-01 22:57:07 +0200

    add some comments on crumbs_RuleWeighKeeper

--- lib/RuleWeightKeeper.php
+++ lib/RuleWeightKeeper.php
@@ -1,6 +1,17 @@
 <?php
 
-
+/**
+ * Can determine a weight for a rule key based on wildcard weights.
+ *
+ * E.g. if the weight settings are
+ *   * = 0
+ *   crumbs.* = 4
+ *   crumbs.nodeParent.* = 5
+ * and then we are looking for a weight for
+ *   crumbs.nodeParent.page
+ * then the weight keeper will return 5, because crumbs.nodeParent.* is the best
+ * matching wildcard.
+ */
 class crumbs_RuleWeightKeeper {
 
   protected $ruleWeights;
@@ -8,11 +19,26 @@ class crumbs_RuleWeightKeeper {
   protected $prefixSorted = array();
   protected $soloSorted = array();
 
+  /**
+   * @param array $rule_weights
+   *   Weights with wildcards, as saved in the configuration form.
+   */
   function __construct(array $rule_weights) {
     asort($rule_weights);
     $this->ruleWeights = $rule_weights;
   }
 
+  /**
+   * Get a "child" weight keeper with a prefix.
+   * E.g. if the config contains a weight setting "crumbs.nodeParent.* = 5"
+   * then in the child keeper this will be just "nodeParent.* = 5".
+   *
+   * @param string $prefix
+   *   The prefix.
+   *
+   * @return crumbs_RuleWeightKeeper
+   *   The prefixed weight keeper.
+   */
   function prefixedWeightKeeper($prefix) {
     if (!isset($this->prefixedKeepers[$prefix])) {
       $this->prefixedKeepers[$prefix] = $this->_buildPrefixedWeightKeeper($prefix);
@@ -20,6 +46,15 @@ class crumbs_RuleWeightKeeper {
     return $this->prefixedKeepers[$prefix];
   }
 
+  /**
+   * Helper: Actually build the prefixed keeper.
+   *
+   * @param string $prefix
+   *   Prefix, as above.
+   *
+   * @return crumbs_RuleWeightKeeper
+   *   The prefixed weight keeper.
+   */
   protected function _buildPrefixedWeightKeeper($prefix) {
     $weights = array();
     $k = strlen($prefix);
@@ -38,6 +73,12 @@ class crumbs_RuleWeightKeeper {
     return new crumbs_RuleWeightKeeper($weights);
   }
 
+  /**
+   * Get the smallest weight in range.
+   *
+   * @return int
+   *   The smallest weight..
+   */
   function getSmallestWeight() {
     foreach ($this->ruleWeights as $weight) {
       if ($weight !== FALSE) {
@@ -49,17 +90,35 @@ class crumbs_RuleWeightKeeper {
 
   /**
    * Determine the weight for the rule specified by the key.
+   *
+   * @param string $key
+   *   Key that we are looking for.
+   *
+   * @return int
+   *   The weight for this key.
    */
   function findWeight($key = NULL) {
     if (!isset($key)) {
+      // Find the top-level weight.
       return $this->ruleWeights[''];
     }
     if (isset($this->ruleWeights[$key])) {
+      // Look for explicit weight setting.
       return $this->ruleWeights[$key];
     }
+    // Try wildcards.
     return $this->_findWildcardWeight($key);
   }
 
+  /**
+   * Helper: Resolve wildcards..
+   *
+   * @param string $key
+   *   Key that we are looking for.
+   *
+   * @return int
+   *   The weight for this key.
+   */
   protected function _findWildcardWeight($key) {
     $fragments = explode('.', $key);
     $partial_key = array_shift($fragments);
