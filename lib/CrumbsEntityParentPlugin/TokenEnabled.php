33aa54305245670d69bd53c46429faa751c3b359
Andreas Hennings <andreas@dqxtech.net>
2013-08-05 21:46:18 +0200

    Move crumbs entity parent plugins into lib

---
+++ lib/CrumbsEntityParentPlugin/TokenEnabled.php
@@ -0,0 +1,36 @@
+<?php
+
+class crumbs_CrumbsEntityParentPlugin_TokenEnabled extends crumbs_CrumbsEntityParentPlugin {
+
+  /**
+   * @inheritdoc
+   */
+  function entityFindParent($entity, $entity_type, $distinction_key) {
+
+    // This is cached..
+    $patterns = variable_get('crumbs_' . $entity_type . '_parent_patterns', array());
+
+    if (!empty($patterns[$distinction_key])) {
+
+      // Use token to resolve the pattern.
+      $info = entity_get_info($entity_type);
+      $parent = token_replace($patterns[$distinction_key],
+        array(
+          $info['token type'] => $entity,
+        ),
+        array(
+          'language' => $GLOBALS['language'],
+          'callback' => 'crumbs_clean_token_values',
+        )
+      );
+
+      if (!empty($parent)) {
+        // Only accept candidates where all tokens are fully resolved.
+        // This means we can't have literal '[' in the path - so be it.
+        if (FALSE === strpos($parent, '[')) {
+          return $parent;
+        }
+      }
+    }
+  }
+}
