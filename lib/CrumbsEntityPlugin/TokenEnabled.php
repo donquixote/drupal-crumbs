1b59095f5138fe7b60859b54728d6258beef8412
Andreas Hennings <andreas@dqxtech.net>
2013-08-13 04:36:33 +0200

    Flatten TokenEnabled plugin class

--- lib/CrumbsEntityPlugin/TokenEnabled.php
+++ lib/CrumbsEntityPlugin/TokenEnabled.php
@@ -10,27 +10,28 @@ class crumbs_CrumbsEntityPlugin_TokenEnabled extends crumbs_CrumbsEntityPlugin_T
     // This is cached..
     $patterns = variable_get('crumbs_' . $entity_type . '_parent_patterns', array());
 
-    if (!empty($patterns[$distinction_key])) {
-
-      // Use token to resolve the pattern.
-      $info = entity_get_info($entity_type);
-      $parent = token_replace($patterns[$distinction_key],
-        array(
-          $info['token type'] => $entity,
-        ),
-        array(
-          'language' => $GLOBALS['language'],
-          'callback' => 'crumbs_clean_token_values',
-        )
-      );
-
-      if (!empty($parent)) {
-        // Only accept candidates where all tokens are fully resolved.
-        // This means we can't have literal '[' in the path - so be it.
-        if (FALSE === strpos($parent, '[')) {
-          return $parent;
-        }
-      }
+    if (empty($patterns[$distinction_key])) {
+      return;
     }
+
+    // Use token to resolve the pattern.
+    $info = entity_get_info($entity_type);
+    $token_data = array($info['token type'] => $entity);
+    $token_options = array(
+      'language' => $GLOBALS['language'],
+      'callback' => 'crumbs_clean_token_values',
+    );
+    $parent = token_replace($patterns[$distinction_key], $token_data, $token_options);
+    if (empty($parent)) {
+      // Token collapsed..
+    }
+
+    // Only accept candidates where all tokens are fully resolved.
+    // This means we can't have literal '[' in the path - so be it.
+    if (FALSE !== strpos($parent, '[')) {
+      return;
+    }
+
+    return $parent;
   }
 }
