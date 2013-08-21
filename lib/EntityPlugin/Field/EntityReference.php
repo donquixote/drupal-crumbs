edcb6ff6e3a7606c944d6dbc2fc3be35ded76ba0
ptvv <ptvv@2674141.no-reply.drupal.org>
2013-08-21 19:46:28 +0200

    entity_uri() in entityreference plugin needs to load entity. See https://drupal.org/node/2070673.

--- lib/EntityPlugin/Field/EntityReference.php
+++ lib/EntityPlugin/Field/EntityReference.php
@@ -7,12 +7,15 @@ class crumbs_EntityPlugin_Field_EntityReference extends crumbs_EntityPlugin_Fiel
    */
   function fieldFindCandidate(array $items) {
     $field = $this->getFieldInfo();
-    foreach ($items as $item) {
-      if (1
-        && !empty($item['target_id'])
-        && $uri = entity_uri($field['settings']['target_type'], $item['target_id'])
-      ) {
-        return $uri['path'];
+    if ($target_type = $field['settings']['target_type']) {
+      foreach ($items as $item) {
+        if (1
+          && !empty($item['target_id'])
+          && ($target_entity = reset(entity_load($target_type, array($item['target_id']))))
+          && $uri = entity_uri($target_type, $target_entity)
+        ) {
+          return $uri['path'];
+        }
       }
     }
   }
