7dd5fb2878ec5ca7c5a0a13c2097ab8a4ffd915b
Andreas Hennings <andreas@dqxtech.net>
2013-12-29 18:08:40 +0100

    crumbs_EntityPlugin_Field_EntityReference->fieldFindCandidate(): reset() can only be called by reference. Fixes https://drupal.org/node/2101609.

--- lib/EntityPlugin/Field/EntityReference.php
+++ lib/EntityPlugin/Field/EntityReference.php
@@ -11,8 +11,8 @@ class crumbs_EntityPlugin_Field_EntityReference extends crumbs_EntityPlugin_Fiel
       foreach ($items as $item) {
         if (1
           && !empty($item['target_id'])
-          && ($target_entity = reset(entity_load($target_type, array($item['target_id']))))
-          && $uri = entity_uri($target_type, $target_entity)
+          && count($target_entities = entity_load($target_type, array($item['target_id'])))
+          && $uri = entity_uri($target_type, reset($target_entities))
         ) {
           return $uri['path'];
         }
